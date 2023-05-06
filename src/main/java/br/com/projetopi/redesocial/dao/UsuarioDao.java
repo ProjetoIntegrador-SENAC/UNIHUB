ba09850e69c45c1c3f651343420a1a5a528c1757
Murilolima98 <murilolimaa1998@gmail.com>
2023-05-05 22:22:36 -0300

    "metodos usariodao"

--- src/main/java/br/com/projetopi/redesocial/dao/UsuarioDao.java
+++ src/main/java/br/com/projetopi/redesocial/dao/UsuarioDao.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UsuarioDao {
     Connection conexao;
@@ -85,6 +86,54 @@ public class UsuarioDao {
         }
 
         return true;
+    }
+
+    public ArrayList<Usuario> findAllPageable(int qtd_elementos, int num_inicio, String email, String senha, String papel) {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        String sqlQuery = "SELECT * FROM usuario WHERE email = ? AND senha = ? AND papel = ? LIMIT ? OFFSET ?;";
+        try (PreparedStatement ps = conexao.prepareStatement(sqlQuery)) {
+            ps.setString(1, email);
+            ps.setString(2, senha);
+            ps.setString(3, papel);
+            ps.setInt(4, qtd_elementos);
+            ps.setInt(5, num_inicio);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Usuario usuario = new Usuario();
+                    usuario.setId(rs.getInt("id"));
+                    usuario.setEmail(rs.getString("email"));
+                    usuario.setSenha(rs.getString("senha"));
+                    usuario.setPapel(rs.getString("papel"));
+                    usuarios.add(usuario);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
+    }
+
+    public Usuario findById(int id) {
+        String sqlQuery = "select * from usuario where id = ?";
+        ResultSet result;
+        Usuario usuario = new Usuario();
+        try {
+            PreparedStatement ps = conexao.prepareStatement(sqlQuery);
+            ps.setInt(1, id);
+            result = ps.executeQuery();
+
+            while (result.next()) {
+                usuario.setEmail(result.getString("email"));
+                usuario.setSenha(result.getString("senha"));
+                usuario.setPapel(result.getString("papel"));
+                usuario.setId(result.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+            return usuario;
+
+
     }
 
 

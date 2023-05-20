686b29667f4f61988dc63196232d0eefd7d92c31
Ronald Garcia <ronaldag58@gmail.com>
2023-05-20 16:43:43 -0300

    update post functionality

--- src/main/java/br/com/projetopi/redesocial/repository/DatabaseInitializer.java
+++ src/main/java/br/com/projetopi/redesocial/repository/DatabaseInitializer.java
@@ -81,7 +81,7 @@ public class DatabaseInitializer implements ServletContextListener {
                 id integer primary key auto_increment,
                 conteudo varchar (500),
                 foto_id integer,
-                data_postagem varchar(4),
+                data_postagem date,
                 conta_id integer,
                 check(foto_id is not null or conteudo is not null),
                 foreign key (foto_id) references foto(id),

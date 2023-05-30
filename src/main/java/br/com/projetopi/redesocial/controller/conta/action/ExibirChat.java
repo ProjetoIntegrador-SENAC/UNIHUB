2d9d5fdb88f9bd0e0dc696225954ab41e469dea9
jrsantos1 <jhonatanjonh@outlook.com>
2023-05-29 19:19:41 -0500

    add dao mensagem

--- src/main/java/br/com/projetopi/redesocial/controller/conta/action/ExibirChat.java
+++ src/main/java/br/com/projetopi/redesocial/controller/conta/action/ExibirChat.java
@@ -1,16 +1,27 @@
 package br.com.projetopi.redesocial.controller.conta.action;
 
 import br.com.projetopi.redesocial.interfaces.Action;
+import br.com.projetopi.redesocial.model.Usuario;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class ExibirChat implements Action {
 
     @Override
     public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession sessao = request.getSession();
+        Usuario usuario = (Usuario) sessao.getAttribute("usuarioLogado");
+        int idContaOrigem = usuario.getId();
+        int idContaDestino = Integer.valueOf(request.getParameter("id_conta_destino"));
+        request.setAttribute("id_conta_origem", idContaOrigem);
+        request.setAttribute("id_conta_destino", idContaDestino);
+
+
+
         return "forward:chat.jsp";
     }
 }
--- src/main/java/br/com/projetopi/redesocial/dao/MensagemDao.java
+++ src/main/java/br/com/projetopi/redesocial/dao/MensagemDao.java
@@ -1,11 +1,14 @@
 package br.com.projetopi.redesocial.dao;
 
 import br.com.projetopi.redesocial.model.Mensagem;
+import br.com.projetopi.redesocial.model.Postagem;
 import br.com.projetopi.redesocial.repository.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MensagemDao {
     private Connection con;
@@ -28,6 +31,34 @@ public class MensagemDao {
             System.out.println(e);
             return false;
         }
+
+    }
+
+    public ArrayList<Mensagem> getMessages(int id_origem,int id_destino){
+        String sqlQuery = "select * from mensagem where ((conta_remente_id = 1 or  conta_destinatario_id = 1) and (conta_remente_id = 2 or  conta_destinatario_id = 2)) order by data_mensagem asc";
+
+        ArrayList<Mensagem> mensagens = new ArrayList<>();
+
+        try(PreparedStatement ps = con.prepareStatement(sqlQuery)){
+            ResultSet resultSet = ps.executeQuery();
+
+            while(resultSet.next()){
+                Mensagem mensagem = new Mensagem();
+
+                mensagem.setConteudo(resultSet.getString("conteudo"));
+                mensagem.setData_mensagem(resultSet.getDate("data_mensagem"));
+                mensagem.setConta_remente_id(resultSet.getInt("conta_remente_id"));
+                mensagem.setConta_destinatario_id(resultSet.getInt("conta_destinatario_id"));
+
+                mensagens.add(mensagem);
+
+            }
+            return mensagens;
+        }catch (SQLException e){
+            System.out.println(e);
+            return null;
+        }
+
 
     }
 
